7d02f77f28cd433d6f4a39cec92bb39f4eadf43f
qaidjoharj53 <qaidjoharytqt@gmail.com>
2024-08-14 09:27:05 +0530

    Training Day-6

---
+++ HTML-CSS/Training/3rd Year/MERN/Day6/index.js
@@ -0,0 +1,17 @@
+console.log(console)
+console.log('js')
+
+//array = []
+//object = {}
+
+//always use camelCase
+
+let firstName = 'Qaidjohar';
+console.log(firstName)
+
+let college;
+console.log(college)
+console.log(typeof college)
+
+let department = null;
+console.log(typeof department);
